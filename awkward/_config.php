945467d0489418f4dede9bb901100b09885a430d
Cameron Grant <sasky.creative@gmail.com>
2014-02-06 14:51:12 +1300

    more initial shizzel

--- awkward/_config.php
+++ awkward/_config.php
@@ -4,6 +4,5 @@ global $project;
 $project = 'awkward';
 i18n::set_locale('en_GB');
 
-
 Config::inst()->update('SiteTree', 'nested_urls', true);
-Config::inst()->update('SSViewer', 'theme', 'akward');
+Config::inst()->update('SSViewer', 'theme', 'awkward');
